c885d953e1463b0b3a83a31421174623c12042f3
Jessica De Mota <jessicademota@Jessicas-MacBook-Pro-2.local>
2020-08-20 01:26:19 -0400

    pushing just in case of errors I noticed at first

--- Module_6_Programming2/js/rock_paper_scissors.js
+++ Module_6_Programming2/js/rock_paper_scissors.js
@@ -1,90 +1,75 @@
 
-const game = () =>
-{
+const game = () =>{
     let pScore = 0;
     let cScore = 0; 
 
     // Start the game
-    const startGame = ( ) =>
-    {
-        const playBtn = document.querySelctor(".intro button");
+    const startGame = ( ) =>{
+        const playBtn = document.querySelector(".intro button");
         const introScreen = document.querySelector(".intro");
         const match = document.querySelector(".match");
 
-        playBtn.addEventListener("click", () =>
-        {
+        playBtn.addEventListener("click", () =>{
             introScreen.classList.add("fadeOut");
             match.classList.add("fadeIn");
         });
+    }
         // Play match 
-        const playMatch = () =>
-    {
+        const playMatch = () => {
             const options = document.querySelectorAll(".options button");
             const playerHand = document.querySelector(".player-hand");
             const computerHand = document.querySelector(".computer-hand");
             const hands = document.querySelectorAll(".hands img");
 
-        hands.forEach(hand =>
-        {
-            hand.addEventListener("animationed", function () 
-            {
+        hands.forEach(hand =>{
+            hand.addEventListener("animationed", function (){
                     this.style.animation = "";
             });
         });
-
+    
         // Computer Options 
         const computerOptions = ['rock', 'paper', 'scissors'];
-        options.forEach(option =>
-        {
-            options.addEventListener("click", function() 
-            {
-                const  computerNumber = Math.floor(Math.random() * 3);
-                const computerChoice = computerOptions[computerNumbers];
+        options.forEach(option =>{
+            option.addEventListener("click", function() {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
 
                 // Update Images 
-                setTimeout(() => 
-                {
-                    compareHands(this.textContent,Computerchoice);
-                    playerHand.src = './assets/$/this.textContent}.png';
-                    computerHand.src = './assets/$/this.ComputerChoice},png';
+                setTimeout(() => {
+                    compareHands(this.textContent, computerChoice);
+                    playerHand.src = `./images/${this.textContent}.png`;
+                    computerHand.src = `./images/${computerChoice}.png`;
 
                 }, 2000);
                 // Animation 
-                playerHand.style.animation = "shakePlayer's 2 ease";
-                computerHand.style.animation = "shakeComputer's 2 ease";
+                playerHand.style.animation = "shakePlayers 2 ease";
+                computerHand.style.animation = "shakeComputers 2 ease";
             });
         });
     };  
     
 
-    const updateScore = ( ) => 
-    {
+    const updateScore = ( ) => {
         const playerScore = document.querySelector(".player-score p")
-        const computerScore = document.querySelctor(".computer-score p")
+        const computerScore = document.querySelector(".computer-score p")
         playerScore.textContent = pScore;
         computerScore.textContent = cScore;
     }
 
-        const compareHands = (playerChoice, computerChoice) => 
-        {
+        const compareHands = (playerChoice, computerChoice) => {
             const winner = document.querySelector('.winner');
 
-            if(playerChoice === computerChoice)
-            {
+            if(playerChoice === computerChoice){
                 winner.textContent = "It is a tie";
                 return;
             }
-            if(playerChoice === 'rock')
-            {
-                if(computerChoice === 'scissors')
-                {
+            if(playerChoice === 'rock'){
+                if(computerChoice === 'scissors'){
                     winner.textContent = "Player Wins"
                     pScore++;
                     updateScore();
                     return;
-                }
-                else 
-                {
+                } else {
                     winner.textContent = "Computer Wins";
                     cScore++;
                     updateScore();
@@ -92,39 +77,32 @@ const game = () =>
                 }
             }
             // Check for paper 
-            if(playerChoice === 'paper')
-            {
-                if(computerChoice === 'scissors')
-                {
+            if(playerChoice === 'paper'){
+                if(computerChoice === 'scissors'){
                     winner.textContent = "Computer  Wins"
                     cScore++;
                     updateScore();
                     return;
-                }
-                else 
-                {
+                }else{
                     winner.textContent = "Player  Wins";
                     pScore++;
                     updateScore();
                     return; 
                 }
             }
-            if(playerChoice === 'scissors')
-            {
-                if(computerChoice === 'rock')
-                {
+            if(playerChoice === 'scissors'){
+                if(computerChoice === 'rock'){
                     winner.textContent = "Computer  Wins"
                     cScore ++;
                     updateScore();
                     return;
-                }
-                else 
-                {
+                }else{
                     winner.textContent = "Player  Wins";
                     pScore++;
                     updateScore();
                     return; 
-                 }
+                }
+            }
             };
 
     startGame();
